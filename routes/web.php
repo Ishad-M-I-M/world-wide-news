6b8de1812009de7462a21bc68870dbc1c953ce55
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2022-06-07 12:28:43 +0530

    bugfix- split subscription array

--- routes/web.php
+++ routes/web.php
@@ -107,18 +107,26 @@ Route::post('/article-admin-view/{id}/{action}', function ($id, $action){
         if($action === "Approved") {
             $time = new DateTime();
             DB::table('articles')->where(['id' => $id])->update(['published at' => $time->format("Y-m-d H:m:s")]);
-        }
-        $category = DB::table('articles')->where(['id'=> $id])->value('category');
-        $category = DB::table('articles')->where(['id'=> $id])->value('category');
-        $users = \App\Models\User::all();
-        foreach ($users as $user){
-            if(in_array($category, $user->subscriptions)){
-                $user->notify(new \App\Notifications\ArticleApproved([
-                    'user' => $user,
-                    'category' => $category,
-                    'id' => $id]));
+            $category = DB::table('articles')->where(['id'=> $id])->value('category');
+            $users = \App\Models\User::all();
+            foreach ($users as $user){
+                $subscriptions = [];
+                $token = strtok($user->subscriptions, ",");
+
+                while ($token !== false)
+                {
+                    $subscriptions[] = $token;
+                    $token = strtok(",");
+                }
+                if(in_array($category, $subscriptions)){
+                    $user->notify(new \App\Notifications\ArticleApproved([
+                        'user' => $user,
+                        'category' => $category,
+                        'id' => $id]));
+                }
             }
         }
+
         return redirect('admin-panel')->with('success', 'Article successfully '.$action);
     }
     else{
